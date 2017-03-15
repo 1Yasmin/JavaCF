7a9575b466f1045c561fdf86063ba89a827dee80
daniel96rb <drodriguezbianchi96@gmail.com>
2017-03-15 08:57:27 -0600

    Update MainProgramadores.java

--- src/MainProgramadores.java
+++ src/MainProgramadores.java
@@ -7,30 +7,26 @@ import java.util.Set;
 import java.util.TreeSet;
 
 /**
- * 
- */
-
-/**
  * @author Yasmin Chavez
- *
+ * @author Daniel Rodriguez
  */
 public class MainProgramadores {
 
-	protected static Set myHash; 
+	protected static Set myHash;
 	/**
 	 * @param args
 	 * Java Collections Framework: Sets
-	 * Interface: Set 
+	 * Interface: Set
 	 * Implementation: HashSet, TreeSet, LinkedHashSet
-	 * 
+	 *
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		FactoryHash factory = new FactoryHash();
 		Conjunto<String, String, String>  miConjunto = new Conjunto<String, String, String>();
-		
-		Scanner sc = new Scanner(System.in); 
+
+		Scanner sc = new Scanner(System.in);
 		//Instanciar objetos a utilizar
 		 String cadena;
 		 System.out.println("Elija el tipo de Hash (HashSet, TreeSet o LinkedHashSet): ");
@@ -47,64 +43,63 @@ public class MainProgramadores {
 			 myHash = factory.getSet(tHash);
 			 System.out.println(myHash);
 		 }
-		 System.out.println("¿Cuantos programadores ingresara?");
+		 System.out.println("Â¿Cuantos programadores ingresara?");
 		 int cantProgra = Integer.parseInt(sc.nextLine());
-		 
+
 		 int cantIngresada = 0;
-		 
-		 
+
+
 		while(cantIngresada != cantProgra){
-			
+
 			System.out.println("Nombre del programador");
 			String name = sc.nextLine();
-			
+
 			System.out.println("DPI del programador");
 			String dpi = sc.nextLine();
-			
+
 			System.out.println("Desarrolla en Java (si/no)");
 			String respJava = sc.nextLine();
 			if(respJava.equals("si")){
-				Conjunto java = new Conjunto("Java", name, dpi);			
+				Conjunto java = new Conjunto("Java", name, dpi);
 				myHash.add(java);
-				System.out.println(myHash);
-				
 			}
 			else{
 				boolean desarrollaJava = false;
 			}
-			
+
 			System.out.println("Desarrolla en Android (si/no)");
 			String respAndroid = sc.nextLine();
 			if(respAndroid.equals("si")){
-				Conjunto android = new Conjunto("Adroid", name, dpi);	
+				Conjunto android = new Conjunto("Android", name, dpi);
 				myHash.add(android);
-				System.out.println(myHash);
 			}
 			else{
 				boolean desarrollaAndroid = false;
 			}
-			
+
 			System.out.println("Desarrolla en iOS (si/no)");
 			String respIos = sc.nextLine();
 			if(respIos.equals("si")){
-				Conjunto iOS = new Conjunto("iOS", name, dpi);	
+				Conjunto iOS = new Conjunto("iOS", name, dpi);
 				myHash.add(iOS);
-				System.out.println(myHash);
 			}
 			else{
 				boolean desarrollaIos = false;
 			}
-			
-			cantIngresada++; 
+
+			cantIngresada++;
 		 }
+
 		 
-		 
-		
+
+
+
 		//Operaciones Requeridas
-		 
-		 
-		
-	
+		System.out.println("\nEl conjunto de los desarrolladores es: ");
+		System.out.println(myHash);
+
+
+
 
 	}
 
