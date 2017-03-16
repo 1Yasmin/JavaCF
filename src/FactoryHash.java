ad497413b88b8988fd21a43691447dba331d493e
daniel96rb <drodriguezbianchi96@gmail.com>
2017-03-15 23:45:45 -0600

    Comentarios

--- src/FactoryHash.java
+++ src/FactoryHash.java
@@ -1,18 +1,24 @@
+/*
+HOJA DE TRABAJO # 6
+Desarrolladores de Java, Android y iOS
+
+Autores:
+Daniel Rodriguez # 15796
+Yasmin Chavez #
+
+Clase FactoryHash permite seleccionar el tipo de ordenamiento a travez
+de las diferentes implementaciones de SET (HashSet, TreeSet, LinkedHashSet)
+utilizando el patron de diseño de Factory.
+*/
+
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.TreeSet;
 
-/**
- * 
- */
-
-/**
- * @author ychav
- *
- */
 public class FactoryHash<E> {
-	
+
+	//Metodo que devuelve la instancia del tipo de SET dependiendo del seleccionado.
 	public Set getSet(String theSet){
 		if (theSet.equals("HashSet")){
 			return new HashSet<Conjunto>();
